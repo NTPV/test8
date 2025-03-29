9aeab33e4eb35a690aecf6ef5977ed1042750497
NTPV <iq36@yandex.ru>
2025-03-30 01:34:05 +0300

    Первый коммит

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+
+import PrimeVue from 'primevue/config';
+import ConfirmationService from 'primevue/confirmationservice';
+import ToastService from 'primevue/toastservice';
+
+createApp(App).mount('#app')
